081b76027b979ce8af1e0b2af4a6b0152e3290c6
Mffedai <121286410+Mffedai@users.noreply.github.com>
2023-12-13 11:57:00 +0300

    Update Main.java

--- VücutKitleIndexi/vucutKitleIndexi/src/Main.java
+++ VücutKitleIndexi/vucutKitleIndexi/src/Main.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        //Girdi değerleri tamamlanır.
         Scanner input = new Scanner(System.in);
-        System.out.print("Lütfen boyunuzu metre cinsinden giriniz:");
-        double boy = input.nextDouble();
+        System.out.print("Lütfen boyunuzu metre cinsinden giriniz:"); 
+        double boy = input.nextDouble(); //Boy bilgisi girilir.
 
         System.out.print("Lütfen kilonuzu giriniz:");
-        double kilo = input.nextDouble();
+        double kilo = input.nextDouble();//Kilo bilgisi girilir.
 
-        double index = kilo / (boy * boy);
+        double index = kilo / (boy * boy);//Sonuç hesaplanır.
 
-        System.out.println("Vücut kitle indexiniz : " + index);
+        System.out.println("Vücut kitle indexiniz : " + index);//.sonuç ekrana yazdırılır.
     }
 }
